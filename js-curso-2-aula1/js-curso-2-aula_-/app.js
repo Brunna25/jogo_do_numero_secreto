4204ed2c94a0829a2b5be4b0c98017204c8fcb28
Brunna Caroline <brunnacaroline187@gmail.com>
2024-10-08 12:33:25 -0300

    Update app.js

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,10 +1,32 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML ='jogo do número secreto'; // quando vai inserir um texto sempre colocar em aspas
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML ='jogo do número secreto'; // quando vai inserir um texto sempre colocar em aspas
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML ='escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML ='escolha um número entre 1 e 10';
+
+let nemeroSecreto = 7;
+
+function exibirTextoNatela(teg, texto) {
+ let campo = document.querySelector(tag);
+ campo.innerHTML = texto;
+}
+
+exibirTextoNatela('h1','jogo do número secreto' );
+exibirTextoNaTela('p', 'escolha um número ente 1 e 10');
 
 function verificarChute() {
-    console.log ('o botão foi clicado')
+    let chute = document.querySelector('input').value;
 
+    if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'acertou!')
+    }
 }
+
+//quando utiliza == é para comparar um valor
+// qano utiliza = é para atribuir um valor
+
+function gerarNumeroAleatorio() {
+   return parseInt (Math.random()* 10 + 1);
+}
+
+// false em javaScript significa que ele é booean pu seja verdadeiro ou falso 
